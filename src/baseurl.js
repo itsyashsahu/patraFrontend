80586f48e94b295ebb9be6698825ba38137bd424
yashsahuglosity <yash.sahu@glosity.club>
2022-05-04 18:53:35 +0530

    updated the backend url

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,3 +1,4 @@
 const baseURL =
-  "http://ip172-18-0-145-c9p5c5g9jotg00dalftg-4000.direct.labs.play-with-docker.com/";
+  // "http://ip172-18-0-11-c9p7pn14lkkg009e1pdg-4000.direct.labs.play-with-docker.com/";
+  "http://ip172-18-0-11-c9p7pn14lkkg009e1pdg-4000.direct.labs.play-with-docker.com/";
 export default baseURL;
